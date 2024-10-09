a6594ce265f8b2ec8b627642569cfb541ba3bef3
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2024-10-09 09:35:04 +0200

    modified:   routes/event.routes.js

--- config/config.js
+++ config/config.js
@@ -1,20 +1,18 @@
-const mysql = require('mysql2');
-const fs = require('fs');
+const mysql = require("mysql2");
+const fs = require("fs");
 
-
-//const sql = fs.readFileSync('./database/sql.sql', 'utf8');
-
+const sql = fs.readFileSync('./database/sql.sql', 'utf8');
 
 const pool = mysql.createPool({
-   host : 'localhost',
-   port : 3306,
-   user : 'tvhevents',
-   password : 'alpb^NPL6]Tj_bIc',
-   database: 'tvhevents',
-   multipleStatements: true, 
+  host: "localhost",
+  port: 3306,
+  user: "chopa",
+  password: "1234",
+  database: "tvhevents",
+  multipleStatements: true,
 });
 
-/*
+
 pool.query(sql, (err, results) => {
     if(err){
         console.log(err)
@@ -24,15 +22,6 @@ pool.query(sql, (err, results) => {
     }
     
 });
-*/
+
 
 module.exports = pool.promise();
-
-
-
-
-
-
-
-
-
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createEvent, deleteEvent, getAllEvents,getEvent,getEventByUser,updateEvent} = require('../controllers/event.controller')
+const {
+  createEvent,
+  deleteEvent,
+  getAllEvents,
+  getEvent,
+  getEventByUser,
+  updateEvent,
+  getEventByCategory,
+} = require("../controllers/event.controller");
 
 router.post('/create', createEvent)
 router.get('/all', getAllEvents)
