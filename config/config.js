c713bbc003eed9d4f90b9835d850e2d2d4dcc6d0
yinhla <makamuyinhla2016@gmail.com>
2024-10-16 14:54:17 +0200

    modified:   config/config.js

--- config/config.js
+++ config/config.js
@@ -6,13 +6,13 @@ const sql = fs.readFileSync('./database/sql.sql', 'utf8');
 const pool = mysql.createPool({
   host: "localhost",
   port: 3306,
-  user: "chopa",
-  password: "1234",
+  user: "tvhevents",
+  password: "alpb^NPL6]Tj_bIc",
   database: "tvhevents",
   multipleStatements: true,
 });
 
-
+/*
 pool.query(sql, (err, results) => {
     if(err){
         console.log(err)
@@ -22,6 +22,6 @@ pool.query(sql, (err, results) => {
     }
     
 });
-
+*/
 
 module.exports = pool.promise();
