e63f529b74acafee8cb3b45b8c551238eed9feaf
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2025-01-24 09:34:58 +0200

    modified:   controllers/attendee.controller.js 	modified:   routes/attendee.routes.js

--- controllers/attendee.controller.js
+++ controllers/attendee.controller.js
@@ -1,178 +1,208 @@
-const bcrypt = require('bcrypt')
-const db = require('../config/config')
-const transporter = require("../config/send_email")
+const bcrypt = require("bcrypt");
+const db = require("../config/config");
+const transporter = require("../config/send_email");
 
+const updateDeviceID = async (req, res) => {
+  const { device_id } = req.body;
 
-const updateDeviceID = async(req, res) =>{
-    const {device_id} = req.body;
-    
-    console.log(device_id)
-}
+  console.log(device_id);
+};
 
-const getAttendeeByEmail = async(req, res) =>{
-    const {email} = req.params;
-    console.log(email);
-    await db.execute("SELECT * from attendee where email = ?", [email]
-    ).then((response) => {
-        return res.status(200).json({results : response[0]})
-    }).catch((error) =>{
-        return res.status(500).json({message : error.message})
+const getAttendeeByEmail = async (req, res) => {
+  const { email } = req.params;
+  console.log(email);
+  await db
+    .execute("SELECT * from attendee where email = ?", [email])
+    .then((response) => {
+      return res.status(200).json({ results: response[0] });
     })
-    
-}
+    .catch((error) => {
+      return res.status(500).json({ message: error.message });
+    });
+};
 
-const sendOtp = async(email, otp) =>{
-    await transporter.sendMail({
-        from : "info.events@7stack.co.za",
-        to : email,
-        subject : "HackTrack Reset Password OTP",
-        html : "<div><h3>Reset your HackTrack login password</h3><p>Use the following OTP to reset your password <br> <strong>" + otp + "</strong></p></div>"
-    }).catch((error) =>{
-        console.log(error)
-        throw error;
+const sendOtp = async (email, otp) => {
+  await transporter
+    .sendMail({
+      from: "info.events@7stack.co.za",
+      to: email,
+      subject: "HackTrack Reset Password OTP",
+      html:
+        "<div><h3>Reset your HackTrack login password</h3><p>Use the following OTP to reset your password <br> <strong>" +
+        otp +
+        "</strong></p></div>",
     })
-}
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+};
 
-const generateOtp = async(req, res) =>{
-    const {email} = req.params;
-    const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-    
-    console.log("Email : " + email);
-  
-    if (!email) {
-      return res.status(400).json({ error: "Email is required" });
-    }
-  
-    try {
-      await sendOtp(email, otp);
-      return res.status(200).json({ otp: otp });
-    } catch (error) {
-      return res.status(500).json({ error: "Failed to send OTP" });
-    }
-  };  
+const generateOtp = async (req, res) => {
+  const { email } = req.params;
+  const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
 
-const getAttendeeEvents = async(req, res) =>{
-    const {attendee_id} = req.params;
+  console.log("Email : " + email);
 
-    await db.execute(
-        "SELECT e.* from event e " + 
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
+  try {
+    await sendOtp(email, otp);
+    return res.status(200).json({ otp: otp });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to send OTP" });
+  }
+};
+
+const getAttendeeEvents = async (req, res) => {
+  const { attendee_id } = req.params;
+
+  await db
+    .execute(
+      "SELECT e.* from event e " +
         "JOIN registration_form rf on rf.event_id = e.event_id " +
-        "JOIN registration r on r.registration_form_id = rf.registration_form_id " + 
+        "JOIN registration r on r.registration_form_id = rf.registration_form_id " +
         "WHERE r.attendee_id = ?",
-        [attendee_id]
-    ).then((response)=>{
-        return res.status(200).json({results : response[0]})
-    }).catch((error) =>{
-        console.log(error)
-        return res.status(500).json({message : error.message})
+      [attendee_id]
+    )
+    .then((response) => {
+      return res.status(200).json({ results: response[0] });
     })
-}
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: error.message });
+    });
+};
 
-const updatePassword = async(req, res) =>{
-    try{
-        const {attendee_id, current_password, new_password} = req.body;
+const updatePassword = async (req, res) => {
+  try {
+    const { attendee_id, current_password, new_password } = req.body;
 
-        const current_user = await db.execute(
-            'SELECT password from attendee where attendee_id = ?',
-            [attendee_id]
-        );
-        const isValid = await bcrypt.compare(current_password, current_user[0][0].password)
-        
-        if(isValid !== true){
-            return res.status(401).json({results : isValid})
-        }
+    const current_user = await db.execute(
+      "SELECT password from attendee where attendee_id = ?",
+      [attendee_id]
+    );
+    const isValid = await bcrypt.compare(
+      current_password,
+      current_user[0][0].password
+    );
 
-        const encryped_password = await bcrypt.hash(new_password,10)
-        await db.execute(
-            'UPDATE attendee set password = ? where attendee_id = ?',
-            [encryped_password, attendee_id]
-        ).then((response) =>{
-            return res.status(200).json({results : isValid}) 
-        })
-
-        
+    if (isValid !== true) {
+      return res.status(401).json({ results: isValid });
     }
-    catch(error){
-        return res.status(500).json({message : error.message})
-    }
-}
 
-const resetPassword = async(req, res) =>{
-    const {email, new_password} = req.body;
+    const encryped_password = await bcrypt.hash(new_password, 10);
+    await db
+      .execute("UPDATE attendee set password = ? where attendee_id = ?", [
+        encryped_password,
+        attendee_id,
+      ])
+      .then((response) => {
+        return res.status(200).json({ results: isValid });
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-    const encryped_password = await bcrypt.hash(new_password,10)
-    await db.execute(
-        'UPDATE attendee set password = ? where email = ?',
-        [encryped_password, email]
-    ).then((response) =>{
-        return res.status(200).json({results : "Password successfully reset"}) 
-    }).catch((error) =>{
-        return res.status(500).json({message : error.message})
+const resetPassword = async (req, res) => {
+  try {
+    const { email, new_password } = req.body;
+
+    const attendee_id = await db.execute(
+      "SELECT attendee_id from attendee where email = ?",
+      [email]
+    );
+
+    const encryped_password = await bcrypt.hash(new_password, 10);
+    await db
+      .execute("UPDATE attendee set password = ? where attendee_id = ?", [
+        encryped_password,
+        attendee_id[0][0].attendee_id,
+      ])
+      .then((response) => {
+        return res.status(200).json({ results: "Password successfully reset" });
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: error.message });
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const signAttendeeRegister = async (req, res) => {
+  const { registration_id } = req.body;
+  console.log("registration id : " + registration_id);
+
+  await db
+    .execute("SELECT successful from registration WHERE registration_id = ?", [
+      registration_id,
+    ])
+    .then((response) => {
+      if (response[0].successful === 1) {
+        return res.status(200).json({ results: "already signed the register" });
+      }
     })
-}
+    .catch((error) => {
+      console.log(error.message);
+      return res.status(500).json({ message: error.message });
+    });
 
-const signAttendeeRegister = async(req, res)=>{
-    const {registration_id} = req.body;
-    console.log("registration id : " + registration_id)
+  await db
+    .execute(
+      "UPDATE registration SET successful = 1 WHERE registration_id = ? ",
+      [registration_id]
+    )
+    .catch((error) => {
+      console.log(error.message);
+      return res.status(500).json({ message: error.message });
+    });
 
-    await db.execute(
-        'SELECT successful from registration WHERE registration_id = ?',
-        [registration_id]
-    ).then((response) =>{
-        if(response[0].successful === 1){
-            return res.status(200).json({results : "already signed the register"})
-        }
-    }).catch((error) =>{
-        console.log(error.message);
-        return res.status(500).json({message : error.message})
+  return res.status(200).json({ results: "Successfully signed register" });
+};
+
+const endAttendeeRegister = async (req, res) => {
+  const { registration_id } = req.params;
+
+  await db
+    .execute("SELECT successful from registration WHERE registration_id = ?", [
+      registration_id,
+    ])
+    .then((response) => {
+      if (response[0].successful === 0) {
+        return res.status(200).json({ results: "Hasn't signed register yet" });
+      }
     })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: error.message });
+    });
 
-    await db.execute(
-        'UPDATE registration SET successful = 1 WHERE registration_id = ? ',
-        [registration_id]
-    ).catch((error) =>{
-        console.log(error.message);
-        return res.status(500).json({message : error.message})
-    })
+  await db
+    .execute(
+      "UPDATE registration SET successful = 0 WHERE registration_id = ? ",
+      [registration_id]
+    )
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: error.message });
+    });
 
-    return res.status(200).json({results : "Successfully signed register"})
-}
-
-const endAttendeeRegister = async(req, res)=>{
-    const {registration_id} = req.params;
-
-    await db.execute(
-        'SELECT successful from registration WHERE registration_id = ?',
-        [registration_id]
-    ).then((response) =>{
-        if(response[0].successful === 0){
-            return res.status(200).json({results : "Hasn't signed register yet"})
-        }
-    }).catch((error) =>{
-        console.log(error);
-        return res.status(500).json({message : error.message})
-    })
-
-    await db.execute(
-        'UPDATE registration SET successful = 0 WHERE registration_id = ? ',
-        [registration_id]
-    ).catch((error) =>{
-        console.log(error);
-        return res.status(500).json({message : error.message})
-    })
-
-    return res.status(200).json({results : "Successfully removed signature from register"}) 
-}
-
+  return res
+    .status(200)
+    .json({ results: "Successfully removed signature from register" });
+};
 
 module.exports = {
-    updateDeviceID,
-    getAttendeeEvents,
-    updatePassword,
-    signAttendeeRegister,
-    endAttendeeRegister,
-    getAttendeeByEmail,
-    generateOtp,
-    resetPassword
-}
-
+  updateDeviceID,
+  getAttendeeEvents,
+  updatePassword,
+  signAttendeeRegister,
+  endAttendeeRegister,
+  getAttendeeByEmail,
+  generateOtp,
+  resetPassword,
+};
--- routes/attendee.routes.js
+++ routes/attendee.routes.js
@@ -1,14 +1,22 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const { updateDeviceID, generateOtp, getAttendeeEvents, resetPassword, getAttendeeByEmail, updatePassword,signAttendeeRegister, endAttendeeRegister } = require('../controllers/attendee.controller')
+const {
+  updateDeviceID,
+  generateOtp,
+  getAttendeeEvents,
+  resetPassword,
+  getAttendeeByEmail,
+  updatePassword,
+  signAttendeeRegister,
+  endAttendeeRegister,
+} = require("../controllers/attendee.controller");
 
-
-router.post("/device", updateDeviceID)
-router.get("/events/:attendee_id", getAttendeeEvents)
-router.put("/password", updatePassword)
-router.put("/events/:registration_id", signAttendeeRegister)
-router.delete("/events/:registration_id", endAttendeeRegister)
-router.get("/email/:email", getAttendeeByEmail)
-router.get("/otp/:email", generateOtp)
-router.put("/resetPassword/:email/:new_password", resetPassword)
+router.post("/device", updateDeviceID);
+router.get("/events/:attendee_id", getAttendeeEvents);
+router.put("/password", updatePassword);
+router.put("/events/:registration_id", signAttendeeRegister);
+router.delete("/events/:registration_id", endAttendeeRegister);
+router.get("/email/:email", getAttendeeByEmail);
+router.get("/otp/:email", generateOtp);
+router.put("/resetPassword/", resetPassword);
 module.exports = router;
